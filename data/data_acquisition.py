16a81633ff7bd17df6cf6e9cfacfb0943d79139a
Lilia Ellouz <lilia.ellouz@epfl.ch>
2020-12-11 19:58:53 +0100

    document data acquisiton, eliminate notebook

--- data/data_acquisition.py
+++ data/data_acquisition.py
@@ -5,7 +5,7 @@ wikipedia.set_lang('en') # setting wikipedia language
 import sys
 from time import sleep
 import re
-
+import regex
 
 def fetch_category_members(category_members, level=0, max_level=1):
         """
@@ -21,12 +21,12 @@ def fetch_category_members(category_members, level=0, max_level=1):
         for c in category_members.values():
             if c.ns == 0:
                 article_names.append(c) 
-                #print("%s: %s (ns: %d)" % ("*" * (level + 1), c.title, c.ns))
             elif level < max_level and c.ns == wikipediaapi.Namespace.CATEGORY:
                 sub_list = []
                 sub_list = fetch_category_members(c.categorymembers, level=level + 1, max_level=max_level)
                 article_names = article_names + sub_list
         return article_names
+    
 
 def get_words(article_names):
         """
@@ -57,7 +57,6 @@ def get_words(article_names):
             sleep(0.25)
         return words_df
 
-
 wiki_wiki = wikipediaapi.Wikipedia('en') # getting articles in english
 # fetching the articles for categories of interest
 people_pages = wiki_wiki.page("Category:People from Venice")
@@ -65,13 +64,41 @@ people_articles = fetch_category_members(people_pages.categorymembers, level = 0
 
 venetian_biographies = get_words(people_articles)
 
-# Clean the data by removing duplicates
 venetian_biographies = venetian_biographies.drop_duplicates().reset_index(drop=True)
 
-# Remove anything between equal signs like "== Overview =="
-# Remove anything between <> signs
+# venetian_biographies.to_csv('venetian_bios_full.csv', index=False) 
+
+# Remove anything after References|Sources|Notes|External links|Works (anything containing works)|Further readings|Gallery|Exhibitions
+# bibliography 394 is a good example: it contains 'Major Works'
+venetian_biographies.biography = venetian_biographies.biography.apply(lambda x: re.split(r"=+ (References|Sources|Notes|External links|Works|Further readings|Gallery|Exhibitions|\w+ works.*?) =+", x)[0])
+
+# Remove anything between equal signs like "== Overview ==" or anything between <> signs
 venetian_biographies = venetian_biographies.biography.str.replace("<(.*?)>|=+ (.*?) =+","").to_frame()
 
-# compression_opts = dict(method='zip',
-#                         archive_name='venetian_bios.csv')  
-venetian_biographies.to_csv('venetian_bios.csv', index=False) 
+
+# Dates: Remove anything containing 1900+
+venetian_biographies_old_dates = venetian_biographies[~venetian_biographies.biography.str.contains(r"[1-2][9][0-9][0-9]|[2][0-9]{3}")]
+
+# Insert a [SEP] to separate prompt from rest of biography
+def insert_sep(s):
+    """
+    Given a text, inserts a separator after the first sentence.
+    """
+    s = regex.sub(r'[^(\p{Common}|\p{Latin})]', u'', s)
+    s = s+"end."
+    s = s.replace("\n"," ")
+    i = s.index(re.findall(r"\w{2,}?\.(?!(?:[^(]*\([^)]*\))*[^()]*\))", s)[0])
+    return s[:i] + s[i:-4].replace(" ", " [SEP] ", 1)
+
+venetian_biographies = venetian_biographies.biography.apply(insert_sep)
+
+venetian_biographies_old_dates = venetian_biographies_old_dates.biography.apply(insert_sep)
+
+# CSV with all articles
+venetian_biographies.to_frame().to_csv('bios_all_dates.csv', index=False) 
+
+# CSV with articles only from before 1900
+venetian_biographies_old_dates.to_frame().to_csv('bios_old_dates.csv', index=False) 
+
+# pd.read_csv('bios_old_dates.csv')
+
